d9ab7d2cdaddcb024f397af7ee7408ab97759f00
khanhn-1040 <nguyen.khanh@sun-asterisk.com>
2020-07-14 21:18:12 +0700

    delete test blade

--- resources/views/test.blade.php
+++
@@ -1,6 +0,0 @@
-@for($num = 0; $num <= 8; $num += 2)
-    <div class="item">
-        <p>{{$newProducts[$num]->name_product}}</p>
-        <p>{{$newProducts[$num + 1]->name_product}}</p>
-    </div>
-@endfor
